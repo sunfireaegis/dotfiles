a9d0d15040f608cc0744379a84748bbd71176682
sunfireaegis <henry_morgan06@mail.ru>
2022-12-27 10:19:37 +0300

    init

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias lsa="ls -la"
+alias ll="ls -l"
+
+alias nigger="gnome-tweaks"
+alias listn="nmcli device wifi list"
+alias connect="nmcli device wifi connect "
